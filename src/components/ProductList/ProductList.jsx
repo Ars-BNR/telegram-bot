cc99a863a0be8c34549e049ac1525c5bafd3233b
Ars-BNR <arslan.galimov2018@yandex.ru>
2025-02-08 11:58:41 +0500

    i

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import "./ProductList.css"
 import { useTelegram } from '../../hooks/useTelegram'
 import ProductItem from '../ProductItem/ProductItem'

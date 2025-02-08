86255d9f8d201c09b3721f5f85ef486da2798848
Ars-BNR <arslan.galimov2018@yandex.ru>
2025-02-08 12:21:19 +0500

    ihg

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -28,6 +28,13 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
+        fetch(`http://localhost:8000`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
     }, [addedItems])
 
     useEffect(() => {
